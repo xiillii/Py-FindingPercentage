df9e306921b6394c8067501ebfd1015d06a20853
José Alonso <mail@josealonso.dev>
2022-08-26 23:45:37 -0500

    Resolving

--- findingPercentage.py
+++ findingPercentage.py
@@ -1,3 +1,7 @@
+def doWork(query_name, student_marks):
+    values = student_marks[query_name]
+    print(sum(values))
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
@@ -6,3 +10,9 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+
+    
+    values = student_marks[query_name]
+    print("%.2f" % float(sum(values)/len(values)))
+
+
